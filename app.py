e343af9058bbb04d901ec7369ee29b5362372586
kkumar47 <kkeee2k4@gmail.com>
2022-08-10 13:46:28 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+
+
+
+
+header = st.container()
+rawdata = st.container()
+
+with header:
+	font="sans serif"
+	textColor="#26273"
+	st.title('Loan Default Prediction System')
+  
+def raw_data():
+	return pd.read_csv('https://raw.githubusercontent.com/kkumar47/Loan_Prediction/master/lending_club_loan_two.csv')
+
+rawdf = raw_data()
+
+with rawdata:
+	st.subheader("Lending Club Loan Dataset", anchor ='The Data')
+	st.text('Raw Data')
+	#The raw data is displayed here
+	st.dataframe(rawdf.head(10))
+	rawd = rawdf.to_csv().encode('utf-8')
+	st.download_button('Download Data', data=rawd, file_name='Raw Data.csv', help='Download Data in CSV format')
